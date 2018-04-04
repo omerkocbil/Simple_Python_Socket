dd6035980968eb1f4a18a3c1318ec613f06f1091
cem dırman <cemdrman@gmail.com>
2018-04-04 15:30:23 +0300

    added funcs

--- server.py
+++ server.py
@@ -7,7 +7,7 @@ import sys
 # typically SOCK_STREAM for connection-oriented protocols and SOCK_DGRAM for connectionless protocols.
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 
-ip_address = "172.16.17.154"
+ip_address = "172.16.17.91"
 socket_port = 3000
 # Bind the socket to the port
 server_address = (ip_address,socket_port)
@@ -16,31 +16,52 @@ print >> sys.stdout, '\tstarting up on %s port %s' % server_address
 sock.bind(server_address)
 
 # Listen for incoming connections
-sock.listen(1)
+sock.listen(0)
 
 while True:
     # Wait for a connection    
     print >> sys.stdout, '\twaiting for a connection'
     print ("#######################################################")
-    #accept() returns an open connection between the server and client, along with the address of the client. 
-    #The connection is actually a different socket on another port (assigned by the kernel). 
-    #Data is read from the connection with recv() and transmitted with sendall().
-    connection, client_address = sock.accept()
+    
     try:
-        print >> sys.stdout, 'connection from', client_address
+        
         # Receive the data in small chunks and retransmit it
+        print("W: Ileri")
+        print("A: Sol")
+        print("S: Geri")
+        print("D: Sag")
+        print ("#######################################################")
+        #accept() returns an open connection between the server and client, along with the address of the client. 
+        #The connection is actually a different socket on another port (assigned by the kernel). 
+        #Data is read from the connection with recv() and transmitted with sendall().
+        connection, client_address = sock.accept()
+        print >> sys.stdout, 'connection from', client_address
         while True:
-            data = '00122223333' #example data for sending to the client
-            print >> sys.stdout, 'sending data  to the client'
-            connection.send(data)
-            print >> sys.stdout, 'sended data  to the client'
-            print >> sys.stdout, 'sending data1 to the client'
-            data1 = '12312343'            
-            connection.send(data1)
-            print >> sys.stdout, 'sended data1 to the client'
+
+            komut = input("Komut Girin(W,A,S,D): ")
+            if komut == "W":
+                forward()
+            elif komut == "A":
+                left()
+            elif komut == "S":
+                backward()
+            elif komut == "D":
+                right()
                        
     finally:
-        # Clean up the connection
-        
+        # Clean up the connection        
         connection.close()
         sock.close()
+
+def forward():
+    connection.send("W")
+    print >> sys.stdout, 'sent data  to the client'
+def left():
+    connection.send("A")
+    print >> sys.stdout, 'sent data  to the client'
+def right():
+    connection.send("D")
+    print >> sys.stdout, 'sent data  to the client'
+def backward():
+    connection.send("S")
+    print >> sys.stdout, 'sent data  to the client'
