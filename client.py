5f8b9bef0d920815a87f70e5b8c6f8389d03ddac
omerkocbil <omer-95@hotmail.com>
2018-04-06 15:19:32 +0300

    client edited

--- client.py
+++ client.py
@@ -1,5 +1,6 @@
 import socket
 import sys
+import car_sample
 
 # Create a TCP/IP socket
 sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
@@ -15,11 +16,22 @@ try:
     message = "00122223333"
     amount_received = 0
     amount_expected = len(message)
+    
     while True:
-       
         data = sock.recv(16)
         amount_received += len(data)
         print(data.decode())
+
+        if data == 'W':
+            car_sample.calis()
+        elif data == 'A':
+            car_sample.dur()
+        elif data == 'D':
+            car_sample.dur()
+        elif data == 'S':
+            car_sample.dur()           
+        else:
+            print "Farklı veri"
 
 finally:
     sock.close()
